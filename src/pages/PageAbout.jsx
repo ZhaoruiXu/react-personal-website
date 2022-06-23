2f5424d0c9d3b88a5d2566fd1b501662ec0fb951
Zhaorui Xu <xzr0429@gmail.com>
2022-06-22 21:11:37 -0700

    pull in all about page about-me information

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -14,14 +14,41 @@ export default function PageAbout() {
       console.log(response.data.acf);
     });
   }, []);
+
   return (
-    <section className='about-page'>
-      <h2>{aboutContent.about_my_name}</h2>
-      <p>{aboutContent.about_my_title}</p>
-      <img
-        src={aboutContent.about_my_illustration.sizes.thumbnail}
-        alt={aboutContent.about_my_illustration.caption}
-      />
-    </section>
+    aboutContent && (
+      <section className='about-page'>
+        <h1 className='scree-reader-text'>About Me</h1>
+        <h2>{aboutContent.about_my_name}</h2>
+        <p>{aboutContent.about_my_title}</p>
+        <img
+          // can get the image size state by useState and matchMedia to update the image size state (might not work yet)
+          src={aboutContent.about_my_illustration.sizes.thumbnail}
+          alt={aboutContent.about_my_illustration.caption}
+        />
+        <h3>{aboutContent.about_skill_header}</h3>
+        {aboutContent.about_skill.map((oneSkillCategory, i) => {
+          return (
+            <section key={i} className='skill-category'>
+              <h4>{oneSkillCategory.skill_category}</h4>
+              <ul>
+                {oneSkillCategory.skills.map((oneSkill, i) => {
+                  return <li key={i}>{oneSkill.individual_skill}</li>;
+                })}
+              </ul>
+            </section>
+          );
+        })}
+        <h3>{aboutContent.about_q_and_a_header}</h3>
+        {aboutContent.about_q_and_a_content.map((oneQAndA, i) => {
+          return (
+            <section key={i} className='skill-category'>
+              <h4>{oneQAndA.question}</h4>
+              <p>{oneQAndA.answer}</p>
+            </section>
+          );
+        })}
+      </section>
+    )
   );
 }

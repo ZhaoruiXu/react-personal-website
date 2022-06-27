e74381630b8b39efe86f0e265145fb75a7b4bce2
Zhaorui Xu <xzr0429@gmail.com>
2022-06-26 19:22:52 -0700

    missing more about me link on homepage

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,5 +1,6 @@
 import { appTitle, api, getAlt, FEATURED, FUN } from "../globals/global";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function PageHome() {
   const [homeAboutContent, setHomeAboutContent] = useState(false);
@@ -14,33 +15,6 @@ export default function PageHome() {
     document.title = `${appTitle} | Front End Developer | UX Desinger`;
 
     const params_about = { acf_format: "standard" };
-
-    const fetchHomeAboutContent = async () => {
-      try {
-        const response_about = await api.get("pages/9", {
-          params: params_about,
-        });
-
-        response_about &&
-          response_about.data &&
-          setHomeAboutContent(response_about.data.acf);
-        console.log("1", response_about.data.acf);
-      } catch (err) {
-        if (err.response) {
-          // not in the 200 response rang
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          // all the other errors
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    };
-    fetchHomeAboutContent();
-  }, []);
-
-  useEffect(() => {
     const params_featured_propject = {
       acf_format: "standard",
       "fwd-project-category": FEATURED,
@@ -49,8 +23,13 @@ export default function PageHome() {
       acf_format: "standard",
       "fwd-project-category": FUN,
     };
-    const fetchHomeProjectContent = async () => {
+
+    const fetchHomeContent = async () => {
       try {
+        const response_about = await api.get("pages/9", {
+          params: params_about,
+        });
+
         const response_featured_project = await api.get("fwd-project", {
           params: params_featured_propject,
         });
@@ -58,6 +37,11 @@ export default function PageHome() {
         const response_fun_project = await api.get("fwd-project", {
           params: params_fun_propject,
         });
+
+        response_about &&
+          response_about.data &&
+          setHomeAboutContent(response_about.data.acf);
+        console.log("1", response_about.data.acf);
 
         if (response_featured_project && response_featured_project.data) {
           setHomeFeaturedProjectContent(response_featured_project.data);
@@ -81,7 +65,8 @@ export default function PageHome() {
         }
       }
     };
-    fetchHomeProjectContent();
+
+    fetchHomeContent();
   }, []);
 
   const handleShowMoreProject = () => {
@@ -97,6 +82,14 @@ export default function PageHome() {
 
   return (
     <section className='home-page'>
+      {homeAboutContent && (
+        <section className='about'>
+          <h2>{homeAboutContent.about_opening_sentence_header}</h2>
+          <p>{homeAboutContent.about_my_title}</p>
+          <Link></Link>
+        </section>
+      )}
+
       <section className='projects-preview'>
         {displayProjects &&
           displayProjects.map((oneProject, index) => {
@@ -113,6 +106,22 @@ export default function PageHome() {
                   {oneProject.acf.project_tools.map((oneTool, index) => {
                     return <li key={index}>{oneTool.project_tool}</li>;
                   })}
+                </ul>
+                <p className='project-description'>
+                  {oneProject.acf.project_preview_description}
+                </p>
+                <ul className='project-links'>
+                  <li>
+                    <a href={oneProject.acf.links[0].live_site_link}>
+                      Live Site
+                    </a>
+                  </li>
+
+                  {oneProject["fwd-project-category"][0] === FEATURED && (
+                    <li>
+                      <Link to={`/project/${oneProject.slug}`}>Learn More</Link>
+                    </li>
+                  )}
                 </ul>
               </article>
             );

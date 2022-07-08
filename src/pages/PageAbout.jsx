d55e9fd8feb7948b364f658e5e13abe7409f0d88
Zhaorui Xu <xzr0429@gmail.com>
2022-07-07 20:38:41 -0700

    add about page scss

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -12,36 +12,40 @@ export default function PageAbout({ aboutContent }) {
     aboutContent && (
       <section className='about-page-section'>
         <h1 className='screen-reader-text'>About page</h1>
-
         <h2>{aboutContent.about_my_name}</h2>
         <h3 className='about-my-title'>{aboutContent.about_my_title}</h3>
         <img
           src={aboutContent.about_my_illustration.sizes.large}
           alt={aboutContent.about_my_illustration.caption}
         />
-
-        <h3>{aboutContent.about_skill_header}</h3>
-        {aboutContent.about_skill.map((oneSkillCategory, i) => {
-          return (
-            <section key={i} className='skill-category'>
-              <h4>{oneSkillCategory.skill_category}</h4>
-              <ul>
-                {oneSkillCategory.skills.map((oneSkill, i) => {
-                  return <li key={i}>{oneSkill.individual_skill}</li>;
-                })}
-              </ul>
-            </section>
-          );
-        })}
-        <h3>{aboutContent.about_q_and_a_header}</h3>
-        {aboutContent.about_q_and_a_content.map((oneQAndA, i) => {
-          return (
-            <section key={i} className='skill-category'>
-              <h4>{oneQAndA.question}</h4>
-              <p>{oneQAndA.answer}</p>
-            </section>
-          );
-        })}
+        <div className='about-skill-wrapper'>
+          <h3>{aboutContent.about_skill_header}</h3>
+          <div className='skill-category-wrapper'>
+            {aboutContent.about_skill.map((oneSkillCategory, i) => {
+              return (
+                <section key={i} className='skill-category'>
+                  <h4>{oneSkillCategory.skill_category}</h4>
+                  <ul>
+                    {oneSkillCategory.skills.map((oneSkill, i) => {
+                      return <li key={i}>{oneSkill.individual_skill}</li>;
+                    })}
+                  </ul>
+                </section>
+              );
+            })}
+          </div>
+        </div>
+        <div className='about-qanda-wrapper'>
+          <h3>{aboutContent.about_q_and_a_header}</h3>
+          {aboutContent.about_q_and_a_content.map((oneQAndA, i) => {
+            return (
+              <article key={i} className='single-qanda-wrapper'>
+                <h4>{oneQAndA.question}</h4>
+                <p>{oneQAndA.answer}</p>
+              </article>
+            );
+          })}
+        </div>
       </section>
     )
   );

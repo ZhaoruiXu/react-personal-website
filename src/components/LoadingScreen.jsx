dd8e0b58cef8f74c3eb595cebf8ec42df81d7f8d
Zhaorui Xu <xzr0429@gmail.com>
2022-07-21 13:54:28 -0700

    change loading screen to use class for closing

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,19 +1,20 @@
 import PuffLoader from "react-spinners/PuffLoader";
 
 export default function LoadingScreen({ playLoadingAnimation, isLoaded }) {
-  if (!isLoaded || playLoadingAnimation) {
-    return (
-      <div className={`loading-animation-wrapper ${!isLoaded ? "close" : ""}`}>
-        <PuffLoader
-          className='loading-animation'
-          cssOverride={{
-            display: "block",
-            position: "absolute",
-          }}
-          color='#ffffff'
-          speedMultiplier={2}
-        />
-      </div>
-    );
-  }
+  return (
+    <div
+      className={`loading-animation-wrapper ${
+        !isLoaded || playLoadingAnimation ? "" : "close"
+      }`}>
+      <PuffLoader
+        className='loading-animation'
+        cssOverride={{
+          display: "block",
+          position: "absolute",
+        }}
+        color='#ffffff'
+        speedMultiplier={2}
+      />
+    </div>
+  );
 }
